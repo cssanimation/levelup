91e4b83e37649c3b097a8f6930575d158d5cbd9e
Donovan Hutchinson <donovanh+git@gmail.com>
2017-03-31 16:23:12 +0100

    Adding module 2 files

--- public/02/02-tooltips/javascripts/tooltips.js
+++ public/02/02-tooltips/javascripts/tooltips.js
@@ -1,11 +1,10 @@
-$(function() {
+/* Function to add tooltips to anchors */
 
-  $('.anchor-tooltip').each(function() {
-    // Prepare the tooltip based on the "title" content
-    var toolTipText = $(this).attr('title'),
-        toolTip = $('<span class="title-container"></span>');
-    toolTip.text(toolTipText);
-    $(this).prepend(toolTip);
-  });
-  
+var anchors = document.querySelectorAll('.anchor-tooltip');
+anchors.forEach(function(anchor) {
+  var toolTipText = anchor.getAttribute('title'),
+      toolTip = document.createElement('span');
+  toolTip.className = 'title-tooltip';
+  toolTip.innerHTML = toolTipText;
+  anchor.appendChild(toolTip);
 });
