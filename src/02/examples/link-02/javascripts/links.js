07e7909eca02131d5817120a7fd597211448d37c
Donovan Hutchinson <donovanh+git@gmail.com>
2017-04-14 12:07:08 +0100

    Updating the examples

--- src/02/examples/link-02/javascripts/links.js
+++ src/02/examples/link-02/javascripts/links.js
@@ -2,7 +2,7 @@
 
 var anchors = document.querySelectorAll('.special-link')
 anchors.forEach(function (anchor) {
-  var linkText = anchor.innerHTML
+  var linkText = anchor.innerText
   console.log('Setting: ', linkText)
   anchor.setAttribute('data-linkText', linkText)
 })
